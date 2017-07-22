f6bfc6237135d90bf9ab9b1c5e151dce9f78e4a1
Saidur Rahman <saidur2k@hotmail.com>
2017-07-22 22:14:29 +1000

    add stack

---
+++ src/stack/Stack.js
@@ -0,0 +1,33 @@
+// @flow
+class Stack<T> {
+  count: number = 0;
+  storage: Array<T> = [];
+
+  size(): number {
+    return this.count || 0;
+  }
+
+  push(data: T): void {
+    this.storage[this.count] = data;
+    this.count += 1;
+  }
+
+  peek(): T {
+    if (this.size === 0) {
+      return this.storage[0];
+    }
+    return this.storage[this.size() - 1];
+  }
+
+  pop(): T {
+    if (this.size() === 0) {
+      throw new Error('Cannot pop empty stack');
+    }
+    const lastItem: T = this.peek();
+    delete this.storage[this.count];
+    this.count -= 1;
+    return lastItem;
+  }
+}
+
+export default Stack;
--- src/stack/Stack.test.js
+++ src/stack/Stack.test.js
@@ -1,1 +1,34 @@
-test('welcome', () => expect(1).toEqual(1));
+import Stack from './Stack';
+
+test('Can push to stack', () => {
+  const myStack = new Stack();
+  expect(myStack.size()).toEqual(0);
+  myStack.push(1);
+  expect(myStack.size()).toEqual(1);
+  expect(myStack.peek()).toEqual(1);
+  myStack.push('abc');
+  expect(myStack.size()).toEqual(2);
+  expect(myStack.peek()).toEqual('abc');
+});
+
+test('Can pop from stack', () => {
+  const myStack = new Stack();
+
+  myStack.push('cat');
+  myStack.push('abc');
+  myStack.push('dog');
+
+  expect(myStack.size()).toEqual(3);
+  expect(myStack.peek()).toEqual('dog');
+  myStack.pop();
+  expect(myStack.size()).toEqual(2);
+  expect(myStack.peek()).toEqual('abc');
+  myStack.pop();
+  expect(myStack.size()).toEqual(1);
+  expect(myStack.peek()).toEqual('cat');
+
+  myStack.pop();
+  expect(myStack.size()).toEqual(0);
+
+  expect(myStack.pop).toThrowError();
+});
